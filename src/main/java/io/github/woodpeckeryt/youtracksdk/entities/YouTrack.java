8e3109bac72a7d28574e3c712ed95ec7dbcff183
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2019-11-21 17:41:26 +0300

    WOODPECKER-5 Авторизация пользователя при помощи логина и пароля

--- src/main/java/io/github/woodpeckeryt/youtracksdk/entities/YouTrack.java
+++ src/main/java/io/github/woodpeckeryt/youtracksdk/entities/YouTrack.java
@@ -2,6 +2,7 @@ package io.github.woodpeckeryt.youtracksdk.entities;
 
 import io.github.woodpeckeryt.youtracksdk.issue.IssueService;
 import io.github.woodpeckeryt.youtracksdk.transport.Transport;
+import io.github.woodpeckeryt.youtracksdk.user.UserService;
 
 public class YouTrack {
     private final String token;
@@ -11,6 +12,7 @@ public class YouTrack {
     private final Transport transport;
 
     private final IssueService issueService;
+    private final UserService userService;
 
     public YouTrack(String token, String host, String projectId) {
         this.token = token;
@@ -19,9 +21,12 @@ public class YouTrack {
 
         this.transport = new Transport(this.token, this.host);
         this.issueService = new IssueService(this.transport, this.projectId);
+        this.userService = new UserService(this.transport);
     }
 
     public IssueService getIssueService() {
         return issueService;
     }
+
+    public UserService getUserService() { return userService; }
 }
