50943f5c2132185beac649dec74728dd450a754d
education20172017@gmail.com <Ltrf,hm_2018>
2020-01-17 06:49:31 +0300

    Added main class

--- src/main/java/io/github/woodpeckeryt/youtracksdk/service/Tracker.java
+++ src/main/java/io/github/woodpeckeryt/youtracksdk/service/Tracker.java
@@ -1,5 +1,6 @@
 package io.github.woodpeckeryt.youtracksdk.service;
 
+import io.github.woodpeckeryt.youtracksdk.entities.YouTrack;
 import io.github.woodpeckeryt.youtracksdk.transport.ApacheTransport;
 import io.github.woodpeckeryt.youtracksdk.transport.Transport;
 
@@ -13,6 +14,14 @@ public class Tracker {
         Transport transport = new ApacheTransport("perm:YW5ncm9uMTIz.NDctNQ==.uLl0ZFOJspRHWbS2ad5vvAOUknmZwp","https://woodpeckeryt.myjetbrains.com/youtrack/api/admin/projects/72d91e0c-9712-4063-8ad8-1fddbea33a6f/issues");
         System.out.println("Test");
         //transport.sendGetRequest();
+
+        YouTrack youTrack = new YouTrack("perm:YW5ncm9uMTIz.NDctNQ==.uLl0ZFOJspRHWbS2ad5vvAOUknmZwp","https://woodpeckeryt.myjetbrains.com/youtrack/api/admin/projects/72d91e0c-9712-4063-8ad8-1fddbea33a6f/issues");
+
+        System.out.println(youTrack.getUserService().getMeInformation());
+        IssueC
+        youTrack.getIssueService().createIssue()
+        System.out.println(youTrack.getIssueService().getIssuesInProject("WOODPECKER"));
+
 
     }
 
