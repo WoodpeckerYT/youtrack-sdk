68daa28e220eb06e444778f536fcbcfa66bcf89f
education20172017@gmail.com <Ltrf,hm_2018>
2020-01-22 23:31:24 +0300

    i have created a method which download all issues from youtrack site method - getAllIssues

--- src/main/java/io/github/woodpeckeryt/youtracksdk/issue/IssueService.java
+++ src/main/java/io/github/woodpeckeryt/youtracksdk/issue/IssueService.java
@@ -2,13 +2,30 @@ package io.github.woodpeckeryt.youtracksdk.issue;
 
 import com.google.gson.Gson;
 import io.github.woodpeckeryt.youtracksdk.issue.dto.IssueCreateDTO;
+import io.github.woodpeckeryt.youtracksdk.transport.ApacheTransport;
 import io.github.woodpeckeryt.youtracksdk.transport.Transport;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class IssueService {
     private final Transport gsonTransport;
 
     public IssueService(Transport gsonTransport) {
         this.gsonTransport = gsonTransport;
+    }
+
+    public Issue[] getAllIssues(String projectId){
+
+        String issuePath = "/api/admin/projects/" + projectId
+                + "/issues?fields=idReadable,created,updated,resolved,numberInProject,project(name),summary,description,usesMarkdown,wikifiedDescription,reporter,updater,draftOwner,isDraft,visibility,votes,comments,commentsCount,tags,links,externalIssue,customFields,voters,watchers,attachments,subtasks,parent";
+        String response = this.gsonTransport.sendGetRequest(issuePath);
+
+        Issue[] issues = new Gson().fromJson(response, Issue[].class);
+
+        System.out.println(issues.length);
+
+        return issues;
     }
 
     public Issue getIssuesInProject(String projectId) {
--- src/main/java/io/github/woodpeckeryt/youtracksdk/service/Tracker.java
+++ src/main/java/io/github/woodpeckeryt/youtracksdk/service/Tracker.java
@@ -1,36 +1,17 @@
 package io.github.woodpeckeryt.youtracksdk.service;
 
 import io.github.woodpeckeryt.youtracksdk.entities.YouTrack;
-import io.github.woodpeckeryt.youtracksdk.issue.dto.IssueCreateCustomFieldDTO;
-import io.github.woodpeckeryt.youtracksdk.issue.dto.IssueCreateDTO;
-import io.github.woodpeckeryt.youtracksdk.issue.dto.IssueCreateProjectDTO;
-import io.github.woodpeckeryt.youtracksdk.transport.ApacheTransport;
-import io.github.woodpeckeryt.youtracksdk.transport.Transport;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Tracker {
 
-
     public static void main(String[] args){
 
-        Transport transport = new ApacheTransport("perm:YW5ncm9uMTIz.NDctNQ==.uLl0ZFOJspRHWbS2ad5vvAOUknmZwp","https://woodpeckeryt.myjetbrains.com/youtrack/api/admin/projects/72d91e0c-9712-4063-8ad8-1fddbea33a6f/issues");
+//        Transport transport = new ApacheTransport("perm:YW5ncm9uMTIz.NDctNQ==.uLl0ZFOJspRHWbS2ad5vvAOUknmZwp","https://woodpeckeryt.myjetbrains.com/youtrack/api/admin/projects/72d91e0c-9712-4063-8ad8-1fddbea33a6f/issues");
 
-        YouTrack youTrack = new YouTrack("perm:YW5ncm9uMTIz.NDctNQ==.uLl0ZFOJspRHWbS2ad5vvAOUknmZwp","https://woodpeckeryt.myjetbrains.com/youtrack/api/admin/projects/72d91e0c-9712-4063-8ad8-1fddbea33a6f/issues");
+       // YouTrack youTrack = new YouTrack("perm:YW5ncm9uMTIz.NDctNg==.LkAsK2z5GOP1VV774PPoMvPrcHrAX8","https://woodpeckeryt.myjetbrains.com/youtrack/api/admin/projects/72d91e0c-9712-4063-8ad8-1fddbea33a6f/issues");
+        YouTrack youTrack = new YouTrack("perm:YW5ncm9uMTIz.NDctNg==.LkAsK2z5GOP1VV774PPoMvPrcHrAX8","https://woodpeckeryt.myjetbrains.com/youtrack");
 
-        youTrack.getIssueService().getIssuesInProject("WOODPECKER");
-        System.out.println(youTrack.getIssueService().getIssuesInProject("WOODPECKER"));
-
-        IssueCreateProjectDTO issueCreateProjectDTO = new IssueCreateProjectDTO("1234");
-        Object object = new Object();
-        IssueCreateCustomFieldDTO issueCreateCustomFieldDTO = new IssueCreateCustomFieldDTO("TestType","TestName",object );
-
-        List<IssueCreateCustomFieldDTO> customFields = new ArrayList<IssueCreateCustomFieldDTO>();
-        customFields.add(issueCreateCustomFieldDTO);
-        IssueCreateDTO issueCreateDTO = new IssueCreateDTO("TestSummary","TestDescription",issueCreateProjectDTO,customFields);
-        youTrack.getIssueService().createIssue(issueCreateDTO);
-
+        youTrack.getIssueService().getAllIssues("0-0");
 
     }
 
